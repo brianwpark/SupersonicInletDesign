cfb0ccc890fcbf467bf41da0a5738985045fdf8b
Brian Woojin Park <69791449+brianwpark@users.noreply.github.com>
2025-03-29 10:25:56 -0400

    Added parameters

--- BaselineDesign.m
+++ BaselineDesign.m
@@ -1,5 +1,15 @@
-function output = InletDesign(input)
-    % Input parameters
-    
+%Baseline parameters
+gamma = 1.385;
+M_mol = 29.1;
+M_mol_KG = Mmol/1000; % kg/mol
+R = 8.314; % J/(mol*K)
+R_Specific = R/MmolKG; % J/(kg*K)
 
-end
+%Lift off parameters
+T_sea = 288.15; % K
+P_sea = 101325; % Pa
+
+%required parameters
+m_dot = 63.6; % kg/s
+
+%lift off calculation
