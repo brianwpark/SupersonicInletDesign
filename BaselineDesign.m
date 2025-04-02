172b6cadfa2478651fa5f4ad78a24df557443de4
Brian Woojin Park <69791449+brianwpark@users.noreply.github.com>
2025-04-02 00:47:30 -0400

    Mid progress of calculating Mass flow rate for supersonic conditions

--- BaselineDesign.m
+++ BaselineDesign.m
@@ -7,12 +7,17 @@ R_Specific = R/MmolKG; % J/(kg*K)
 
 Turn_angle = 5.5; % degrees
 
-%Lift off parameters
+%Liftoff conditions
 T_sea = 288.15; % K
 P_sea = 101325; % Pa
-M_inlet_liftoff = 0.55;
+M_inlet_entrance_liftoff = 0.55;
 M_vehicle_liftoff = 0.31;
 m_dot = 63.6; % kg/s
+
+%Supersonic cruise conditions
+P_ambient = 11.6e3; % Pa
+T_ambient = 216e3; % K
+M_inlet_exit_suprsonic = 0.55;
 
 %Baseline Design Parameters
 Turn_angle = 5.5; % degrees
@@ -35,5 +40,8 @@ H1 = 2.5*S; % m
 Turn_angle_rad = deg2rad(5.5); % radians
 L = H1*tan(Turn_angle_rad); % m
 
-%Exit height calculation
+%Exit height calculation (currently thinking)
 
+%Supersonic inlet calculation
+%Mass flow rate calculation
+a = sqrt(gamma*R_Specific*T_ambient); % m/s
