ac67e6fcd9a34cba8133f2385d4496af811a0572
Brian Woojin Park <69791449+brianwpark@users.noreply.github.com>
2025-04-02 11:32:48 -0400

    Added basic parameters for BaselineOffDesign calculation

---
+++ BaselineOffDesign.m
@@ -0,0 +1,24 @@
+%Design Requirements
+gamma = 1.385;
+M_mol = 29.1;
+M_mol_KG = Mmol/1000; % kg/mol
+R = 8.314; % J/(mol*K)
+R_Specific = R/MmolKG; % J/(kg*K)
+
+Turn_angle = 5.5; % degrees
+
+%Liftoff conditions
+T_sea = 288.15; % K
+P_sea = 101325; % Pa
+M_inlet_entrance_liftoff = 0.55;
+M_vehicle_liftoff = 0.31;
+m_dot = 63.6; % kg/s
+
+%Supersonic cruise conditions
+P_ambient = 11.6e3; % Pa
+T_ambient = 216e3; % K
+M_inlet_exit_suprsonic = 0.55;
+M_vehicle_supersonic = 1.60;
+
+%Baseline Design Parameters
+Turn_angle = 5.5; % degrees
