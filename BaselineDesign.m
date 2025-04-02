345d0e07911c957c1e80d3719c8ee6cda6fe93ad
Brian Woojin Park <69791449+brianwpark@users.noreply.github.com>
2025-04-02 11:24:22 -0400

    Finished calculating Baseline Design except for H2

--- BaselineDesign.m
+++ BaselineDesign.m
@@ -18,6 +18,7 @@ m_dot = 63.6; % kg/s
 P_ambient = 11.6e3; % Pa
 T_ambient = 216e3; % K
 M_inlet_exit_suprsonic = 0.55;
+M_vehicle_supersonic = 1.60;
 
 %Baseline Design Parameters
 Turn_angle = 5.5; % degrees
@@ -25,13 +26,13 @@ Turn_angle = 5.5; % degrees
 %lift off calculation
 %Area calculation
 %speed of sound calculation
-a = sqrt(gamma*R_Specific*T_sea); % m/s
+a_liftoff = sqrt(gamma*R_Specific*T_sea); % m/s
 %Velocity calculation
-v = M_inlet_liftoff*a; % m/s
+V_ = M_inlet_liftoff*a; % m/s
 %density calculation
 rho = P_sea/(R_Specific*T_sea); % kg/m^3
 %Area calculation
-A = m_dot/(rho*v); % m^2
+A_entrance = m_dot/(rho*v); % m^2
 %Height and Width calculation
 S = sqrt(A/2.5); % m
 H1 = 2.5*S; % m
@@ -44,4 +45,9 @@ L = H1*tan(Turn_angle_rad); % m
 
 %Supersonic inlet calculation
 %Mass flow rate calculation
-a = sqrt(gamma*R_Specific*T_ambient); % m/s
+a_supersonic = sqrt(gamma*R_Specific*T_ambient); % m/s
+V_Supersonic = M_inlet_exit_suprsonic*a_supersonic; % m/s
+Rho_supersonic = P_ambient/(R_Specific*T_ambient); % kg/m^3
+m_dot_supersonic = Rho_supersonic*V_Supersonic*A_entrance; % kg/s
+%Stagnation pressure calculation
+P2_supersonic = (((2*gamma)/(gamma+1))*(M_vehicle_supersonic^2)*(sin(Turn_angle_rad)^2)-((gamma-1)/(gamma+1)))*P_ambient; % Pa
