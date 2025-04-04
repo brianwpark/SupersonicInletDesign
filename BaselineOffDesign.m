587dd73cd0ffcbe11d453ae3effee1ebb08f8a93
Brian Park <brian.woojin.park@gmail.com>
2025-04-03 21:07:44 -0400

    Added varification for Attached shock

--- BaselineOffDesign.m
+++ BaselineOffDesign.m
@@ -1,9 +1,15 @@
+%{
+    This code is for calculating stagnation temperature and pressure, fractional stagnation pressure loss, and mass flow rate entering the inlet.
+%}
+
+
+
 %Design Requirements
 gamma = 1.385;
 M_mol = 29.1;
-M_mol_KG = Mmol/1000; % kg/mol
+M_mol_KG = M_mol/1000; % kg/mol
 R = 8.314; % J/(mol*K)
-R_Specific = R/MmolKG; % J/(kg*K)
+R_Specific = R/M_mol_KG; % J/(kg*K)
 
 %Liftoff conditions
 T_sea = 288.15; % K
@@ -19,12 +25,18 @@ M_inlet_exit_suprsonic = 0.55;
 M_vehicle_supersonic = 1.60;
 
 %Baseline Design Parameters
-Turn_angle = 5.5; % degrees
+Turn_angle_actual = deg2rad(5.5); % rad
 
 %Detached or Attached Shock
 %https://math.stackexchange.com/questions/4699275/finding-maximum-deflection-angle-for-oblique-shock-waves
+syms shock_wave_angle_rad
+Turn_angle = atan(2*cot(shock_wave_angle_rad)*(((M_vehicle_supersonic)^2*sin(shock_wave_angle_rad)^2-1)/((M_vehicle_supersonic)^2*(gamma+cos(2*shock_wave_angle_rad))))); %rad
+d_turn_d_shock = diff(Turn_angle, shock_wave_angle_rad);
+d_turn_d_shock_func = matlabFunction(d_turn_d_shock); % Convert symbolic function to numeric function handle
+Solved_shock_angle = fzero(d_turn_d_shock_func, [0.1, pi/2]);
+Max_turn_angle = atan(2*cot(Solved_shock_angle)*(((M_vehicle_supersonic)^2*sin(Solved_shock_angle)^2-1)/((M_vehicle_supersonic)^2*(gamma+cos(2*Solved_shock_angle)))));
 
-if Turn_angle > Max_turn_angle
+if Turn_angle_actual > Max_turn_angle
     disp('The oblique shock is a Detached Shock')
 else
     disp('The oblique shock is a Attached Shock')
